721a486ba805f3643c7ab8d27c2a4316c29e8542
XeNoXTS <palmyunvisate2003@gmail.com>
2023-02-07 15:57:17 +0700

    Lab16_5 Completed

--- lab16_5.cpp
+++ lab16_5.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int &,int &, int &, int &); //Modify input arguments to pointer 
+void shuffle(int * ,int *, int *, int *);
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
@@ -11,11 +11,22 @@ int main(){
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d); //Modify input arguments to pointer 
+	    shuffle(&a,&b,&c,&d);
 	    cout << a << " " << b << " " << c << " " << d << "\n";
 	}
 	
 	return 0;
 }
 
-//Write definition of shuffle() using pointer here 
+void shuffle(int *a, int *b, int *c, int *d){
+	int wallet[] = {*a, *b, *c, *d};
+	int n = sizeof(wallet)/sizeof(int);
+	for(int i = 0; i < n; i++){
+		int x = rand()%4;
+		int y = rand()%4;
+		int temp = wallet[x];
+		wallet[x] = wallet[y];
+		wallet[y] = temp;
+	}
+	*a = wallet[0],*b = wallet[1], *c = wallet[2], *d = wallet[3];
+}
